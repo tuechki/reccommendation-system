dae2be0ff6cf1beb398760a5c5d12135ab6ecaa1
tuechki <dtuechki@gmail.com>
2024-01-21 21:15:06 +0200

    Add functionality to update user profile.

--- app/models/User.php
+++ app/models/User.php
@@ -36,6 +36,37 @@
             }
         }
 
+        public function updateUserProfile($data){
+            $this->db->query("UPDATE users_details SET 
+            specialtiesAndCourses = :specialtiesAndCourses,
+            oks = :oks,
+            year = :year,
+            fn = :fn,
+            interests = :interests
+            WHERE userId = :id");
+
+            $this->db->bind(':specialtiesAndCourses', $data['specialtiesAndCourses']);
+            $this->db->bind(':oks', $data['oks']);
+            $this->db->bind(':year', $data['year']);
+            $this->db->bind(':fn', $data['fn']);
+            $this->db->bind(':interests', $data['interests']);
+            $this->db->bind(':id', $data['id']);
+
+            if($this->db->execute()){
+                return true;
+            } else {
+                echo "fail";
+                return false;
+            }
+        }
+
+        public function getUserProfileById($id){
+            $this->db->query("SELECT * FROM users_details WHERE userId = :id");
+            $this->db->bind(':id', $id);
+
+            return $this->db->single();
+        }
+
         // Find user by email
         public function findUserByEmail($email){
             $this->db->query("SELECT * FROM users WHERE email = :email");
